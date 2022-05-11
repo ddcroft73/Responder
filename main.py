7109155f6bc8cfac71c682463b8e1b5e49558812
ddcroft73 <gen.disarray73@outlook.com>
2022-05-11 00:03:40 -0400

    Fixed some error checking problems.

--- main.py
+++ main.py
@@ -14,41 +14,30 @@ from constants import(
 )
 from os import getpid
 
-# Make it send a text to let user know the texts have stopped?
-# Kind of defeats the purpose.
-
 def main(args=None) -> None:
     hunting:          bool = True
-    responder:   Responder = Responder(SET_FILE, getpid())  
+    responder:   Responder = Responder(SET_FILE, getpid())  # so Tizzle can shut it down.
     
     try:
         while(hunting):            
            responder.log.log_task('Checking email...')
            loggedin: bool = responder.email.login()
-
+           
            if loggedin:
               email_message: str = responder.email.get_email_delete_email() 
-              
+
               if email_message:
                  email_data: dict[str, str] = responder.parse_email(email_message)
                  responder.handle_instructions(email_data)                          
            else: 
-               # Login error show POP UP so user checks to see whats wrong.
-                responder.log.log_error_report(
-                    f'Failure to log in to email @ {responder.settings["imap_credentials"][0]}',
-                    f'Could not login to {responder.settings["imap_credentials"][0]} Check your settngs. ',
-                    report=f'Error logging in to: {responder.settings["imap_credentials"][0]}'
-                    )  
-                hunting = False
+                raise Exception('Check login credintials in settings.json file.')
 
            responder.log.log_task('No email, waiting...')
            sleep(WAIT_TIME)    
 
     except Exception as er:
-        # log the error
-        # terminate the app
-        hunting = False
-        responder.log.log_error_report(f'Exception! \n{er}', report=er)
+        hunting = False        
+        responder.log.log_error_report(str(er), report=er)
         exit()
 
     except KeyboardInterrupt:    
